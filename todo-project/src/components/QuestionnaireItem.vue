c01c43197837a4c3afcccdfbde8923031a6c444b
CHARPENTIER Maxym <o22103433@info23-27.iut45.univ-orleans.fr>
2023-04-03 16:53:05 +0200

    Ajout du css

--- todo-project/src/components/QuestionnaireItem.vue
+++ todo-project/src/components/QuestionnaireItem.vue
@@ -4,67 +4,29 @@ export default {
     questionnaire: Object,
     index: Number
   },
-  /*
-  data() {
-    return {
-      editTitle: this.question.title,
-    };
-  },*/
+
   methods:{
     suppr: function() {
     this.$emit("suppr", this.index);
   }
-    /*
-    edit: function() {
-        this.question.editing = true;
-    },
-    applyEdit: function() {
-        this.question.title = this.editText;
-        this.question.editing = false;
-    },
-    suppr: function() {
-    this.$emit("suppr", this.index);
-    }*/
+
 
   },
-  /*
-  emits: ["suppr"]*/
 }
 </script>
 
 <template>
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-     <div class="checkbox">
-        <label>
-        <!-- 
-            <input type="checkbox"
-                v-model="question.checked">
-            <div v-if="question.editing" class="editing">
-                <input type="text" v-model="editText" @keyup.enter="applyEdit()" @blur="applyEdit()" />
-            </div>-->
-            <div>
-                {{ questionnaire.name }}
-            </div>
-        </label>
-        <span class="input-group-btn">
-            <input class="btn btn-danger" value="Supprimer" type="button" @click="suppr()">
-
-        </span>
-        <span class="input-group-btn">
-          <router-link :to="/questions/+questionnaire.id">Voir mes questions</router-link>
-
-        </span>
-        
-        <!-- 
-        <span class="input-group-btn">
-            <button @click="edit()" v-if="!question.editing">Modifier</button>
-
-        </span>
-        <div class="alert alert-success" v-if="question.checked">
-            Done
-        </div>-->
-
+     <div class="checkbox btn-group">
+      <label>
+        <div>{{ questionnaire.name }}</div>
+      </label>
+      <span>
+        <button class="btn btn-danger" @click="suppr()">Supprimer</button>
+        <router-link :to="/questions/+questionnaire.id" class="btn btn-primary">Voir mes questions</router-link>
+      </span>
     </div>
+
    
     
 
