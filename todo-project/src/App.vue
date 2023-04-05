7db4cab17e86db3a4f9fe4447c71978a3a1652c1
JORY Jonathan <o22106917@info07-18.iut45.univ-orleans.fr>
2023-04-05 15:28:02 +0200

    style

--- todo-project/src/App.vue
+++ todo-project/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Questionnaires</router-link>
+    <router-link to="/">
+      <i class="fas fa-home"></i> Accueil
+    </router-link>
   </nav>
   <router-view></router-view>
 </template>
--- todo-project/src/components/QuestionnaireList.vue
+++ todo-project/src/components/QuestionnaireList.vue
@@ -50,27 +50,33 @@ export default{
 </script>
 
 <template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+
   <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
         crossorigin="anonymous">
-  <div class="container">
-            <h2> {{ title }} </h2>
-            <ol>
-                
-                <li v-for="(item, indexx) in questionnaires">
-                    <QuestionnaireItem 
-                    :questionnaire="item"
-                    :index="indexx"
-                    @suppr="suppr($event)"
-                    >
-                    </QuestionnaireItem>
-                </li>
-            </ol>
+  <div class="container d-flex justify-content-center">
+    <div class="row">
+      <div class="col-md-8">
+        <h2>{{ title }}</h2>
+        <ol>
+          <li v-for="(item, indexx) in questionnaires">
+            <QuestionnaireItem 
+              :questionnaire="item"
+              :index="indexx"
+              @suppr="suppr($event)"
+            ></QuestionnaireItem>
+          </li>
+        </ol>
+      </div>
+      <div class="col-md-4 text-end">
+        <router-link to="/questionnaire/add" class="btn btn-default"><i class="fas fa-plus"></i> Ajouter un questionnaire</router-link>
+      </div>
+    </div>
+  </div>
+</template>
 
-        </div>
-        <router-link to="/questionnaire/add" class="btn btn-default">Ajouter un questionnaire</router-link>
-      </template>
 
 <style>
 #app {
